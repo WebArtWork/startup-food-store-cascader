adcf4cdc1bf2d0d04b767295c053758db872725d
Nazar <bogushnazar@gmail.com>
2024-04-04 20:43:19 +0300

    improved design for mobile devices on products page, header and footer

--- js/index.js
+++ js/index.js
@@ -2,30 +2,54 @@ let mouseDown = false;
 let startX, scrollLeft;
 const slider = document.querySelector('.header-f4-scrollable');
 
+const elementHeight = document.querySelector('header').offsetHeight;
+document.documentElement.style.setProperty('--dynamic-height', `${elementHeight}px`);
+
+document.querySelectorAll('.nav-item').forEach(tab => {
+  tab.addEventListener('click', function() {
+    const targetText = this.textContent.trim();
+    const targetElement = document.getElementById(targetText);
+    if (targetElement) {
+
+      targetElement.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+
+    }
+  });
+});
+
+
 const startDragging = (e) => {
+  if (e.type === 'touchstart') {
+    startX = e.touches[0].pageX - slider.offsetLeft;
+  } else {
+    startX = e.pageX - slider.offsetLeft;
+  }
   mouseDown = true;
-  startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 }
 
-const stopDragging = (e) => {
+const stopDragging = () => {
   mouseDown = false;
 }
 
 const move = (e) => {
   e.preventDefault();
-  if(!mouseDown) { return; }
-  const x = e.pageX - slider.offsetLeft;
-  const scroll = x - startX;
+  if (!mouseDown) { return; }
+  let x;
+  if (e.type === 'touchmove') {
+    x = e.touches[0].pageX - slider.offsetLeft;
+  } else {
+    x = e.pageX - slider.offsetLeft;
+  }
+  const scroll = (x - startX) * 1.5;
   slider.scrollLeft = scrollLeft - scroll;
 }
 
 slider.addEventListener('mousemove', move, false);
 slider.addEventListener('mousedown', startDragging, false);
 slider.addEventListener('mouseup', stopDragging, false);
-slider.addEventListener('mouseleave', stopDragging, false)
+slider.addEventListener('mouseleave', stopDragging, false);
 
-const elementHeight = document.querySelector('header').offsetHeight;
-document.documentElement.style.setProperty('--dynamic-height', `${elementHeight}px`);
-
-
+slider.addEventListener('touchmove', move, false);
+slider.addEventListener('touchstart', startDragging, false);
+slider.addEventListener('touchend', stopDragging, false);
